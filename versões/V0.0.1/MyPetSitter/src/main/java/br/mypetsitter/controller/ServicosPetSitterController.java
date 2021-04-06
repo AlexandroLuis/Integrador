089e4a1a5523b6ae397044cabf600b2a83102c74
Alexandro <kasebrotmitkaffee@gmail.com>
2021-04-06 14:18:38 -0300

    Update 1
    deixar apenas a versão final, acaba tendo um peso desnecessário as versões passadas.
    - Comentar o código, Fazer update da recuperação de senha

--- versões/V0.0.1/MyPetSitter/src/main/java/br/mypetsitter/controller/ServicosPetSitterController.java
+++
@@ -1,7 +0,0 @@
-package br.mypetsitter.controller;
-
-public class ServicosPetSitterController {
-
-
-
-}
--- versões/V0.0.1/MyPetSitter/src/main/java/br/mypetsitter/model/Main.java
+++
@@ -1,7 +0,0 @@
-package br.mypetsitter.model;
-
-public class Main {
-	public static void main(String[] args) {
-		LoginFXML.main(args);
-	}
-}
